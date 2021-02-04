c2a06bfa92a0e05d16b0c4ae9470717d3313afdc
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2021-02-04 19:07:02 +0900

    p164まで

--- reverse_polish_notation/src/main.rs
+++ reverse_polish_notation/src/main.rs
@@ -1,35 +1,97 @@
-use clap::{App,Arg};
-//cargo install clap なのか add　七日
+use structopt::StructOpt;
+use core::panic;
+use std::fs::File;
+use std::io::{BufRead,BufReader,stdin};
 
-fn main()
-{
+#[derive(StructOpt,Debug)]
+#[structopt(
+    name="hahaha",
+    version="1.0.0",
+    //auther="me",
+    about="Super RPN"
+)]
+struct Opts{
 
-    let matches=App::new("My RPN pragram")
-    .version("1.0.0")
-    .author("aa")
-    .about("Super awesome ample RPN calculator")
-    .arg(
-        Arg::with_name("formula_file")
-        .help("Formulas written in RPN")
-        .value_name("FILE")
-        .index(1)
-        .required(false)
-    )
-    .arg(
-        Arg::with_name("verbose")
-        .help("Sets the level og verbosity")
-        .short("v")
-        .long("verbose")
-        .required(false)
-    )
-    .get_matches();
+    #[structopt(short,long)]
+    verbose: bool,
 
-    match matches.value_of("formula_file") {
-        Some(file) => println!("File specified: {}",file),
-        None => println!("No file specified."),
+    #[structopt(name="FILE")]
+    formula_file: Option<String>,
+}
+
+fn main(){
+
+    let opts=Opts::from_args();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        //BufReaderから読み込んだ方が良い
+        let reader=BufReader::new(f);
+
+        //readerは参照を渡す借用の方がいいんじゃあないのか？
+       run(reader,opts.verbose);
+    }else {
+        let stdin=stdin();
+        let reader = stdin.lock();
+        run(reader,opts.verbose);
+    }
+}
+
+fn run<R: BufRead>(reader: R,verbose: bool){
+    let calc=RpnCalculator::new(verbose);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let answer=calc.eval(&line);
+        println!("{}",answer);
+    }
+}
+
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self{
+        Self(verbose)
     }
 
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified: {}",verbose);
+    pub fn eval(&self,formula: &str) -> i32  {
+        let mut tokens=formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
 
+    fn eval_inner(&self,tokens: &mut Vec<&str>) -> i32 {
+        let mut stack=Vec::new();
+
+        while let Some(token) = tokens.pop(){
+            if let Ok(x) = token.parse::<i32>(){
+                stack.push(x);
+            }else{
+                let y=stack.pop().expect("invalid syntax");
+                let x=stack.pop().expect("invalid syntax");
+                let res= match token{
+                    "+" => x+y,
+                    "-" => x-y,
+                    "/" => x/y, 
+                    "%" => x%y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res);
+            }
+
+            if self.0{
+                println!("{:?} {:?}",tokens,stack);
+            }
+        }
+
+        if stack.len() == 1{
+            stack[0]
+        }else{
+            panic!("invalid syntax");
+        }
+    }
 }
+
+
+
+
