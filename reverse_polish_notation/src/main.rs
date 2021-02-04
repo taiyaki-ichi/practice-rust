2c7af8b668f1364331fb7d98aba6938783964f3d
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2021-02-04 15:33:46 +0900

    clapその１

--- reverse_polish_notation/src/main.rs
+++ reverse_polish_notation/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{App,Arg};
+//cargo install clap なのか add　七日
+
+fn main()
+{
+
+    let matches=App::new("My RPN pragram")
+    .version("1.0.0")
+    .author("aa")
+    .about("Super awesome ample RPN calculator")
+    .arg(
+        Arg::with_name("formula_file")
+        .help("Formulas written in RPN")
+        .value_name("FILE")
+        .index(1)
+        .required(false)
+    )
+    .arg(
+        Arg::with_name("verbose")
+        .help("Sets the level og verbosity")
+        .short("v")
+        .long("verbose")
+        .required(false)
+    )
+    .get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("File specified: {}",file),
+        None => println!("No file specified."),
+    }
+
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified: {}",verbose);
+
 }
